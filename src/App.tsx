bac573af6160bf1c2ce895b63f1ba45c3b41ccc4
Caleb Martin <calebmartin2020@gmail.com>
2025-03-22 15:52:39 -0500

    in progress: working on general layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { ReactNode } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="base-layout">
+        <SideBar>
+          <Nav />
+        </SideBar>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
 
+const SideBar = ({ children }: { children: ReactNode }) => {
+  return (
+    <div id="sideBar">
+      <div id="sideBarHeader">
+        <a href="" id="sideBarLogo"></a>
+        <p id="sideBarTitle">Dashboard</p>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+const Nav = () => {
+  return (
+    <nav id="sideBarNav">
+      <a href="" id="homePage">
+        Home
+      </a>
+      <a href="" id="profilePage">
+        Profile
+      </a>
+      <a href="" id="settingsPage">
+        Settings
+      </a>
+      <a href="" id="supportPage">
+        Support
+      </a>
+      <a href="" id="privacyPage">
+        Privacy
+      </a>
+    </nav>
+  );
+};
 export default App;
